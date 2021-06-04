9885ad92a468483c939e06c18935d64b4bfa6225
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-06-04 10:44:56 +0100

    add db config

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,9 +1,21 @@
-
-module.exports.HELLO = (req,res)=>{
-  res.send('Hello word !')
+module.exports.HELLO = (req, res) => {
+    res.send('Hello word !')
 }
 
-module.exports.SEND_DATA = (req,res)=>{
+module.exports.SEND_DATA = (req, res) => {
     console.log(req.body)
     res.send('OK!')
+}
+
+module.exports.ADD_MOVIE = (req, res) => {
+
+}
+module.exports.ALL_MOVIES = (req, res) => {
+
+}
+module.exports.EDIT_MOVIE = (req, res) => {
+
+}
+module.exports.DELETE_MOVIE = (req, res) => {
+    
 }
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {HELLO,SEND_DATA} = require("../controllers/movies.controller");
+const {HELLO, SEND_DATA, ADD_MOVIE, ALL_MOVIES, EDIT_MOVIE, DELETE_MOVIE} = require("../controllers/movies.controller");
 
 
-router.get('/hello',HELLO)
-router.post('/send',SEND_DATA)
-
+router.get('/hello', HELLO)
+router.post('/send', SEND_DATA)
+router.post('/add', ADD_MOVIE)
+router.get('/all', ALL_MOVIES)
+router.put('/edit', EDIT_MOVIE)
+router.delete('/delete', DELETE_MOVIE)
 module.exports = router;
