aecc1522ef9268d372dcd43d20fb5d41e690cddb
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-06-04 09:54:52 +0100

    add db config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const db = require('./config/db')
 const api = require('./routes/api')
 //express rendre les api disponible
 const app = express();
